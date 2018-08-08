eab1a460b1037af354f6d76c28c4bce420e6e395
devblok <l.praninskas@gmail.com>
2018-08-08 14:48:40 +0300

    Partial reimplementation of original hackathon code

--- main.go
+++ main.go
@@ -1,17 +1,57 @@
 package main
 
 import (
+	"fmt"
+	"image"
+	"image/color"
 	"os"
 
 	"gocv.io/x/gocv"
 )
 
-var template gocv.Mat = gocv.IMRead("template-intersection.png", gocv.IMWritePngStrategyFiltered)
-var red_line gocv.Mat = gocv.IMRead("template-intersection-lines.png", gocv.IMWritePngStrategyFiltered)
+var template = gocv.IMRead("template-intersection.png", gocv.IMWritePngStrategyFiltered)
+var redLines = gocv.IMRead("template-intersection-lines.png", gocv.IMWritePngStrategyFiltered)
 
 func main() {
-	_, err := gocv.VideoCaptureFile(os.Args[1])
+	capture, err := gocv.VideoCaptureFile(os.Args[1])
 	if err != nil {
 		panic(err)
+	}
+
+	//backgroundSubtractor := gocv.NewBackgroundSubtractorKNN()
+
+	originalWindow := gocv.NewWindow("Original")
+	//regionWindow := gocv.NewWindow("Region")
+	//regionWindow.ResizeWindow(300, 300)
+
+	regionSet := false
+	var regionRect image.Rectangle
+	var rectangleColor = color.RGBA{255, 255, 255, 0}
+
+	cancelled := false
+	for !cancelled {
+		frame := gocv.NewMat()
+		frameOk := capture.Read(&frame)
+		if !frameOk {
+			break
+		}
+
+		if !regionSet {
+			regionResult := gocv.NewMat()
+			templateDims := template.Size()
+			gocv.MatchTemplate(frame, template, &regionResult, gocv.TmCcoeff, gocv.NewMat())
+			_, _, _, maxLoc := gocv.MinMaxLoc(regionResult)
+			fmt.Printf("Region found: [%d, %d]\n", maxLoc.X, maxLoc.Y)
+			regionRect.Min = maxLoc
+			regionRect.Max = image.Point{X: maxLoc.X + templateDims[0], Y: maxLoc.Y + templateDims[1]}
+		}
+
+		gocv.Rectangle(&frame, regionRect, rectangleColor, 2)
+
+		originalWindow.IMShow(frame)
+		if originalWindow.WaitKey(200)&0xFF == 'r' {
+			rectangleColor = color.RGBA{255, 0, 0, 0}
+			regionSet = true
+		}
 	}
 }
