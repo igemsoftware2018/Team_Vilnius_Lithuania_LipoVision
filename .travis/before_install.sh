5522e883a9fea36e9c41a942b8fb34ad25532e27
devblok <l.praninskas@gmail.com>
2018-08-27 11:00:25 +0300

    Change QT version to 4

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then 
-  brew install s3cmd qt5
+  brew install s3cmd qt4
 else
   wget http://netix.dl.sourceforge.net/project/s3tools/s3cmd/2.0.2/s3cmd-2.0.2.tar.gz
   tar xvfz s3cmd-2.0.2.tar.gz
