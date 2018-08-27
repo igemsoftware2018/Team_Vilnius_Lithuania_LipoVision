0e723a15d774eec5bdeac2dc0bdb57d997df3d7e
devblok <l.praninskas@gmail.com>
2018-08-27 11:15:44 +0300

    Install qt4 via tap

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then 
-  brew install s3cmd qt4
+  brew install s3cmd
+  brew tap cartr/qt4
+  brew tap-pin cartr/qt4
+  brew install qt@4
 else
   wget http://netix.dl.sourceforge.net/project/s3tools/s3cmd/2.0.2/s3cmd-2.0.2.tar.gz
   tar xvfz s3cmd-2.0.2.tar.gz
