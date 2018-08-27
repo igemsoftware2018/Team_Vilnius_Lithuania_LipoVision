b27641e6cd5b1706a3c923e47652dbe6a1e94537
devblok <l.praninskas@gmail.com>
2018-08-27 10:49:35 +0300

    Move to script

---
+++ .travis/before_install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then 
+  brew install s3cmd qt5
+else
+  wget http://netix.dl.sourceforge.net/project/s3tools/s3cmd/2.0.2/s3cmd-2.0.2.tar.gz
+  tar xvfz s3cmd-2.0.2.tar.gz
+  cd s3cmd-2.0.2
+  sudo python setup.py install
+  cd ..
+fi
