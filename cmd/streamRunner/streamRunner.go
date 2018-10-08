dc57bf8e2d82170aed18c63faee1754b6245b8b2
devblok <l.praninskas@gmail.com>
2018-10-08 10:27:57 +0300

    Add proper signal handling to tool

--- cmd/streamRunner/streamRunner.go
+++ cmd/streamRunner/streamRunner.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/Vilnius-Lithuania-iGEM-2018/lipovision/device/dropletgenomics"
 	"gocv.io/x/gocv"
@@ -14,8 +16,21 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	stream := dropletDevice.Stream(ctx)
 
+	signalBuffer := make(chan os.Signal, 1)
+	signal.Notify(signalBuffer, os.Interrupt)
+	go func() {
+		for sig := range signalBuffer {
+			fmt.Printf("signal: %s\n", sig)
+			cancel()
+			break
+		}
+	}()
+
+Processing:
 	for {
 		select {
+		case <-ctx.Done():
+			break Processing
 		case frame := <-stream:
 			mat, err := gocv.ImageToMatRGB(frame.Frame())
 			if err != nil {
