c1da479d806a00920b5ed2f3063dbae6fc34479e
devblok <l.praninskas@gmail.com>
2018-10-10 10:49:49 +0300

    Add gotk3 hello world

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+
+	"github.com/gotk3/gotk3/gtk"
 )
 
 func main() {
@@ -11,6 +14,36 @@ func main() {
 	 	 > dropletgenomics
 		  > video`)
 	flag.Parse()
+
+	gtk.Init(nil)
+
+	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	if err != nil {
+		log.Fatal("Unable to create window:", err)
+	}
+	win.SetTitle("Simple Example")
+	win.Connect("destroy", func() {
+		gtk.MainQuit()
+	})
+
+	// Create a new label widget to show in the window.
+	l, err := gtk.LabelNew("Hello, gotk3!")
+	if err != nil {
+		log.Fatal("Unable to create label:", err)
+	}
+
+	// Add the label to the window.
+	win.Add(l)
+
+	// Set the default window size.
+	win.SetDefaultSize(800, 600)
+
+	// Recursively show all widgets contained in this window.
+	win.ShowAll()
+
+	// Begin executing the GTK main loop.  This blocks until
+	// gtk.MainQuit() is run.
+	gtk.Main()
 
 	//processor := processor.CreateFrameProcessor()
 	fmt.Printf(*deviceRequested)
