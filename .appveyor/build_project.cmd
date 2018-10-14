55011e254edc769b16b57c55c4f0190d5284f5cc
devblok <l.praninskas@gmail.com>
2018-10-14 22:18:28 +0300

    Fix build again

--- .appveyor/build_project.cmd
+++ .appveyor/build_project.cmd
@@ -8,4 +8,4 @@ cd ..\cmd\lipovision
 go build -ldflags "-w -H windowsgui" -o ../../build/LipoVision.exe
 
 cd ../..
-c:\msys64\usr\bin\env MSYSTEM=MINGW64 c:\msys64\usr\bin\bash %APPVEYOR_BUILD_FOLDER%\.appveyor\msys_assemble.sh %APPVEYOR_BUILD_FOLDER%\build
+c:\msys64\usr\bin\env MSYSTEM=MINGW64 c:\msys64\usr\bin\bash -l %APPVEYOR_BUILD_FOLDER%\.appveyor\msys_assemble.sh %APPVEYOR_BUILD_FOLDER%\build
