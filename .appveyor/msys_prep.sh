aea0a80c03b701c2db53c536e781c1561abda30b
devblok <l.praninskas@gmail.com>
2018-10-14 21:52:31 +0300

    Add dep for msys2

--- .appveyor/msys_prep.sh
+++ .appveyor/msys_prep.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Deps
-pacman -Sy --needed mingw-w64-x86_64-gtk2 mingw-w64-x86_64-toolchain mingw-w64-x86_64-pkg-config
+pacman -Sy --needed mingw-w64-x86_64-gtk2 mingw-w64-x86_64-gtk3 mingw-w64-x86_64-pkg-config
 
 # Fix pkg-config
 pushd /c/msys64/mingw64/lib/pkgconfig
