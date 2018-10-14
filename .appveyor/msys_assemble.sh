042ecffc6bd9db05df4af905139242f82e05754f
devblok <l.praninskas@gmail.com>
2018-10-14 23:13:43 +0300

    Assemble GTK themes and loaders

--- .appveyor/msys_assemble.sh
+++ .appveyor/msys_assemble.sh
@@ -9,3 +9,12 @@ for dep in ${lines[*]}
 do
     cp $dep $BUILD_FOLDER
 done
+
+# Generaate GTK loaders
+/mingw64/bin/gdk-pixbuf-query-loaders > /mingw64/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache
+
+mkdir -p $BUILD_FOLDER/lib/gdk-pixbuf-2.0/
+mkdir -p $BUILD_FOLDER/share/icons/
+
+cp -r /mingw64/lib/gdk-pixbuf-2.0/2.10.0/ $BUILD_FOLDER/lib/gdk-pixbuf-2.0/
+cp -r /mingw64/share/icons/Adwaita/ $BUILD_FOLDER/share/icons/
